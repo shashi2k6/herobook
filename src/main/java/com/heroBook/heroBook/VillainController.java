d0bc0710e4b77aee037411e9d47b58417a41d827
shashi <shashi2k6@gmail.com>
2021-01-13 19:36:31 -0500

    Added comments for the testcase

--- src/main/java/com/heroBook/heroBook/VillainController.java
+++ src/main/java/com/heroBook/heroBook/VillainController.java
@@ -32,7 +32,7 @@ public class VillainController {
             return new ResponseEntity("Villain doesn't exist", HttpStatus.OK);
     }
 
-    @DeleteMapping("/api/cleanuphero")
+    @DeleteMapping("/api/cleanupvillian")
     public void cleanUpDB(){
         villainService.cleanUpDB();
     }
